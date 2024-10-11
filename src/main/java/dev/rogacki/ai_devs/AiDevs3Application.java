ae0b9320c175bcccd11feac66c4e1d5eea5ec233
Arkadiusz Rogacki <arek.rogacki@gmail.com>
2024-10-11 20:56:34 +0200

    Initial commit

---
+++ src/main/java/dev/rogacki/ai_devs/AiDevs3Application.java
@@ -0,0 +1,13 @@
+package dev.rogacki.ai_devs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDevs3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDevs3Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rogacki/ai_devs/AiDevs3ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rogacki.ai_devs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDevs3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
